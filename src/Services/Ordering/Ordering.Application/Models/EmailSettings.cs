2eee4228e85989e8c821714e754c49e4fd9a5cb0
ritzm1974 <36458226+ritzm1974@users.noreply.github.com>
2021-08-30 08:05:18 -0700

    Ordering.API part 1

---
+++ src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Models
+{
+    class EmailSettings
+    {
+        public string ApiKey { get; set; }
+        public string FromAddress { get; set; }
+        public string FromName { get; set; }
+    }
+}
