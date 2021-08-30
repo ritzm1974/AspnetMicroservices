fc06cbc154a96861c191ed0fffe22a4afa506396
rmonterrey <ritzmonterrey@gmail.com>
2021-08-30 22:20:53 +0300

    add command handler request

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Command/CheckoutOrder/CheckoutorderCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Command.CheckoutOrder
+{
+    public class CheckoutorderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutorderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{Username} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{Username} must not exceed 50 characters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+
+
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,10 +1,7 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Command.CheckoutOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Application.Mappings
 {
@@ -13,7 +10,7 @@ namespace Ordering.Application.Mappings
         public MappingProfile()
         {
             CreateMap<Order, OrdersVm>().ReverseMap();
-            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
             //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
