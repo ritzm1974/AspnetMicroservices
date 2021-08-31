68b908aabebaf9cce468865e339f6332abca2d98
ritzm1974 <36458226+ritzm1974@users.noreply.github.com>
2021-08-31 16:04:08 -0700

    Implemented Event Bus , using RabbitMQ and MassTransit

--- src/Services/Basket/Basket.Api/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -1,6 +1,9 @@
-﻿using Basket.Api.Entities;
+﻿using AutoMapper;
+using Basket.Api.Entities;
 using Basket.Api.GrpcServices;
 using Basket.Api.Repositories;
+using EventBus.Message.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net;
@@ -14,13 +17,15 @@ namespace Basket.Api.Controllers
     {
         private readonly IBasketRepository _repository;
         private readonly DiscountGrpcService _discountGrpcService;
-        //private readonly IPublishEndpoint _publishEndpoint;
-        //private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint, IMapper mapper)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
             _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
+            _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -53,6 +58,39 @@ namespace Basket.Api.Controllers
         {
             await _repository.DeleteBasket(userName);
             return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            //get existing basket with total price
+            //Create basketcheckoutEvent- Set Totalprice on basketcheckout eventmessage
+            //send checkout event to rbbitmq
+            //remove the basket
+
+            //get existing basket with total price
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+
+
+
+            //send checkout event to rbbitmq
+             var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMessage);
+
+
+            //remove the basket
+            await _repository.DeleteBasket(basketCheckout.UserName);
+
+            return Accepted();
+
         }
     }
 }
---
+++ src/Services/Ordering/Ordering.API/EvenBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using EventBus.Message.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Command.CheckoutOrder;
+using System;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EvenBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediator.Send(command);
+
+            _logger.LogInformation("BasketCheckoutEvent consumed succesfully. Created Order Id : {newOrder.Id} ", result);
+        }
+    }
+}
